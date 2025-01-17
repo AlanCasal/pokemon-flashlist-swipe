b0e4725dd9734021a04d847954b0bf33667f287b
Alan Casal <alan.casal.dev@gmail.com>
2025-01-17 07:57:14 -0300

    Home - Added pokemon fetch list

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { Text, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import styles from './styles';
+import { API_URL } from '@/src/constants/api';
+import { Pokemon } from '@/src/types/pokemonList';
 
 const Home = () => {
+	const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+	const [next, setNext] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		const getPokemonList = async () => {
+			const response = await fetch(API_URL);
+			const data = await response.json();
+			setPokemonList(data.results);
+			setNext(data.next);
+		};
+
+		getPokemonList();
+	}, []);
+
+	const handleLoadMore = async () => {
+		if (isLoading || !next) return;
+
+		setIsLoading(true);
+
+		const response = await fetch(next);
+		const data = await response.json();
+		setPokemonList(prevPokemonList => [...prevPokemonList, ...data.results]);
+		setNext(data.next);
+
+		setIsLoading(false);
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>Home</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<FlatList
+				data={pokemonList}
+				renderItem={({ item }) => <Text>{item.name}</Text>}
+				keyExtractor={({ name }, index) => name + index.toString()}
+				onEndReached={handleLoadMore}
+				onEndReachedThreshold={0.5}
+				ListFooterComponent={isLoading ? <ActivityIndicator /> : null}
+			/>
+		</SafeAreaView>
 	);
 };
 
