546172a0da3d5f94efbce7a52f39ad0b6d6e0b94
Alan Casal <alan.casal.dev@gmail.com>
2025-01-17 15:57:35 -0300

    Performance imp

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView, FlatList, ActivityIndicator, Alert } from 'react-native';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './styles';
 import { API_URL } from '@/src/constants/api';
 import { Pokemon } from '@/src/types/pokemonList';
@@ -13,6 +13,7 @@ const Home = () => {
 	const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 	const [next, setNext] = useState<string>(API_URL); // https://pokeapi.co/api/v2/pokemon?offset=20&limit=20
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
 
 	const fetchPage = async (url: string) => {
 		if (isLoading) return;
@@ -32,41 +33,42 @@ const Home = () => {
 	};
 
 	const handleRefresh = async () => {
+		setIsRefreshing(true);
+
 		setPokemonList([]);
 		setNext(API_URL);
 		fetchPage(API_URL);
+
+		setIsRefreshing(false);
 	};
 
 	const handleRenderItem = useCallback(({ item }: { item: Pokemon }) => {
 		return <PokemonCard url={item.url} />;
 	}, []);
 
-	const viewabilityConfigCallbackPairs = useMemo(
-		() => [
-			{
-				viewabilityConfig: {
-					// minimumViewTime: 500,
-					itemVisiblePercentThreshold: 50,
-				},
-				onViewableItemsChanged: ({
-					changed,
-				}: {
-					changed: Array<{ isViewable: boolean; item: Pokemon }>;
-				}) => {
-					changed.forEach(
-						(changedItem: { isViewable: boolean; item: Pokemon }) => {
-							changedItem.isViewable &&
-								console.log(
-									'\x1b[33m\x1b[44m\x1b[1m[changedItem]\x1b[0m',
-									changedItem.item.url
-								);
-						}
-					);
-				},
+	const viewabilityConfigCallbackPairs = useRef([
+		{
+			viewabilityConfig: {
+				// minimumViewTime: 500,
+				itemVisiblePercentThreshold: 50,
 			},
-		],
-		[]
-	);
+			onViewableItemsChanged: ({
+				changed,
+			}: {
+				changed: Array<{ isViewable: boolean; item: Pokemon }>;
+			}) => {
+				changed.forEach(
+					(changedItem: { isViewable: boolean; item: Pokemon }) => {
+						changedItem.isViewable &&
+							console.log(
+								'\x1b[33m\x1b[44m\x1b[1m[changedItem]\x1b[0m',
+								changedItem.item.url
+							);
+					}
+				);
+			},
+		},
+	]);
 
 	useEffect(() => {
 		fetchPage(API_URL);
@@ -78,26 +80,40 @@ const Home = () => {
 	return (
 		<SafeAreaView style={styles.container}>
 			<FlatList
+				// data
 				data={pokemonList}
 				renderItem={handleRenderItem}
-				onEndReached={() => fetchPage(next)}
 				ListFooterComponent={
 					isLoading ? <ActivityIndicator size="large" /> : null
 				}
-				onEndReachedThreshold={0.7}
-				keyExtractor={({ name }, index) => name + index.toString()}
-				refreshing={isLoading}
-				onRefresh={handleRefresh}
-				initialNumToRender={5}
-				debug
+				// content container style
 				contentContainerStyle={styles.contentContainer}
+				// pull to refresh
+				refreshing={isRefreshing}
+				onRefresh={handleRefresh}
+				// onEndReached load more items
+				onEndReached={() => fetchPage(next)}
+				onEndReachedThreshold={1}
+				// Optimizations
+				keyExtractor={({ name }, index) => name + index.toString()}
+				initialNumToRender={5}
 				getItemLayout={(_, index) => ({
-					length: POKEMON_CARD_HEIGHT,
+					length: POKEMON_CARD_HEIGHT, // pre calculation about items height, offset and index
 					offset: CARD_OFFSET * index,
 					index,
 				})}
-				viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs}
+				viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
+				//
+				// onScroll, can animate to change opacity when items are leaving the screen
+				// onScroll={({ nativeEvent }) => {
+				// 	console.log(nativeEvent.contentOffset);
+				// }}
+				// example with a 2 columns list
+				// numColumns={2} // also add flex: 1 to itemContainer style
+				// columnWrapperStyle={{ gap: CARDS_GAP }} // horizontal gap, between the items in the same row
+				//
 				// windowSize={9}
+				debug // debugger -> shows items rendered on the side
 			/>
 		</SafeAreaView>
 	);
