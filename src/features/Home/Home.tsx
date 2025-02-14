e35d01e0bf04f687447578684078fdb36a54f018
Alan Casal <alan.casal.dev@gmail.com>
2025-02-14 13:21:27 -0300

    Home - added styles to marquee, remvoed indent rule

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,14 +1,20 @@
-import { View, Dimensions } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React, { useMemo } from 'react';
-import styles from './styles';
+import styles, { SPACING } from './styles';
 import { SPRITE_URL, TOTAL_POKEMON_COUNT } from '@/src/constants/api';
 import { Image } from 'expo-image';
 import { Marquee } from '@animatereactnative/marquee';
+import { typeColors } from '../../constants/colors';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import Animated, {
+	FadeInDown,
+	FadeInLeft,
+	FadeInRight,
+} from 'react-native-reanimated';
 
-const { width } = Dimensions.get('window');
-const ITEM_SIZE = width * 0.55;
-const SPACING = 8;
-
+const BG_COLOR = typeColors.dragon;
+const MARQUEE_SPEED = 0.5;
 const chunkArray = (array: string[], size: number) => {
 	const chunkArr = [];
 	let index = 0;
@@ -20,6 +26,8 @@ const chunkArray = (array: string[], size: number) => {
 
 	return chunkArr;
 };
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 const Home = () => {
 	const initialArray = useMemo(() => {
@@ -37,31 +45,79 @@ const Home = () => {
 
 	if (images.length === 0) return null;
 
+	const enteringAnimation = (columnIndex: number) => {
+		const delay = 300 * (columnIndex + 1) + Math.random() * 100;
+
+		return columnIndex % 2 === 0
+			? FadeInRight.duration(500).delay(delay)
+			: FadeInLeft.duration(500).delay(delay);
+	};
+
 	return (
 		<View style={styles.container}>
-			<View style={{ gap: SPACING }}>
-				{images.map((column, columnIndex) => (
-					<Marquee
-						key={`marquee-${columnIndex}`}
-						spacing={SPACING}
-						reverse={columnIndex % 2 !== 0}
-					>
-						<View style={[styles.spritesContainer, { gap: SPACING }]}>
-							{column.map((image, imageIndex) => (
-								<Image
-									key={`image-column-${columnIndex}-${imageIndex}`}
-									source={image}
-									style={{
-										width: ITEM_SIZE,
-										aspectRatio: 1,
-										backgroundColor: '#fafafa',
-										borderRadius: SPACING,
-									}}
-								/>
-							))}
-						</View>
-					</Marquee>
-				))}
+			<StatusBar style='light' />
+
+			<View style={styles.headerContainer}>
+				<View style={styles.marqueesContainer}>
+					{images.map((column, columnIndex) => (
+						<Marquee
+							key={`marquee-${columnIndex}`}
+							spacing={SPACING}
+							reverse={columnIndex % 2 !== 0}
+							speed={MARQUEE_SPEED}
+						>
+							<View style={styles.spritesContainer}>
+								{column.map((image, imageIndex) => (
+									<Animated.Image
+										key={`image-column-${columnIndex}-${imageIndex}`}
+										source={{ uri: image }}
+										entering={enteringAnimation(columnIndex)}
+										style={styles.marqueeImage}
+									/>
+								))}
+							</View>
+						</Marquee>
+					))}
+				</View>
+
+				<LinearGradient
+					colors={[BG_COLOR, BG_COLOR, 'transparent']}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 0, y: 1 }}
+					locations={[0, 0.15, 1]}
+					style={[styles.gradient, { top: 0 }]}
+					pointerEvents='none'
+				/>
+
+				<LinearGradient
+					colors={['#00000000', BG_COLOR, BG_COLOR]}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 0, y: 1 }}
+					locations={[0, 0.7, 1]}
+					style={[styles.gradient, { bottom: 0 }]}
+					pointerEvents='none'
+				/>
+			</View>
+
+			<View style={styles.bottomContainer}>
+				<Image
+					source={require('@/assets/images/pokedex-logo.png')}
+					contentFit='contain'
+					style={styles.logo}
+				/>
+				<Text style={styles.description}>
+					Search for any Pokémon {'\n'} that exists on the planet !
+				</Text>
+
+				<AnimatedPressable
+					onPress={() => {}}
+					entering={FadeInDown.springify().damping(12).delay(300)}
+					style={styles.button}
+				>
+					<View style={styles.buttonContent}>
+						<Text style={styles.buttonText}>Start</Text>
+					</View>
+				</AnimatedPressable>
 			</View>
 		</View>
 	);
--- src/features/Home/styles.ts
+++ src/features/Home/styles.ts
@@ -1,10 +1,74 @@
-import { StyleSheet } from 'react-native';
+import { pokeballColors, textColor, typeColors } from '@/src/constants/colors';
+import { PRIMARY_FONT } from '@/src/constants/sharedStyles';
+import { Dimensions, StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+const ITEM_SIZE = width * 0.45;
+export const SPACING = 8;
 
 export default StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: typeColors.dragon,
+		overflow: 'hidden',
+	},
+	headerContainer: {
+		flex: 1,
+		overflow: 'hidden',
+	},
+	marqueesContainer: {
+		flex: 1,
+		gap: SPACING,
+		transform: [{ rotate: '-4deg' }],
 	},
 	spritesContainer: {
 		flexDirection: 'row',
+		gap: SPACING,
+	},
+	marqueeImage: {
+		width: ITEM_SIZE,
+		aspectRatio: 1,
+		backgroundColor: '#fafafa',
+		borderRadius: SPACING,
+	},
+	gradient: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		height: '25%',
+	},
+	bottomContainer: {
+		flex: 0.5,
+		alignItems: 'center',
+		gap: SPACING,
+	},
+	logo: {
+		width: 200,
+		height: 80,
+	},
+	description: {
+		fontFamily: PRIMARY_FONT,
+		fontSize: 18,
+		textAlign: 'center',
+		color: textColor.primary,
+	},
+	button: {
+		marginTop: SPACING * 2,
+	},
+	buttonContent: {
+		height: 48,
+		width: 200,
+		borderRadius: 32,
+		backgroundColor: pokeballColors.red,
+		borderWidth: 2,
+		borderColor: pokeballColors.black,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	buttonText: {
+		color: textColor.primary,
+		fontFamily: PRIMARY_FONT,
+		fontSize: 18,
 	},
 });
