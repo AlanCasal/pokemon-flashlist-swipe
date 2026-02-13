fb2093bc40530232a39232d034b70809de0089c7
Alan Casal <alan.casal.dev@gmail.com>
2026-02-14 00:11:13 +0100

    added Theo tryCatch helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -18,3 +18,45 @@ export const toTransparent = (hexColor: string) => {
 	const b = Number.parseInt(sanitized.slice(4, 6), 16);
 	return `rgba(${r}, ${g}, ${b}, 0)`;
 };
+
+type TryCatchSuccess<T> = { success: true; data: T };
+type TryCatchError = { success: false; error: unknown };
+type TryCatchResult<T> = TryCatchSuccess<T> | TryCatchError;
+
+/**
+ * A utility function to handle asynchronous operations with try-catch.
+ * It returns an object indicating success or failure, along with the data or error.
+ *
+ * @param operation - A function that returns a Promise of type T.
+ * @returns A Promise that resolves to a TryCatchResult containing either the data or the error.
+ * USAGE:
+
+const result = await tryCatch(async () => {
+	return promise
+});
+
+if(result.success) {
+	handle success
+} else {
+	if(result.error instanceof MyError) {
+		handle MyError
+	} else {
+		handle other errors
+	}
+}
+*/
+
+export const tryCatch = async <T>(
+	operation: () => Promise<T>
+): Promise<TryCatchResult<T>> => {
+	try {
+		const data = await operation();
+		return { success: true, data };
+	} catch (error) {
+		return { success: false, error };
+	}
+};
+
+/*
+
+*/
